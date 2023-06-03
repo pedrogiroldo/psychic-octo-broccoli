822d60f7c54fcbfc31e230f018dfb5dd38c7b688
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-06-03 20:36:34 -0300

    modo giras

---
+++ content.js
@@ -0,0 +1,26 @@
+function numeroAleatorio (a, b) {
+    return Math.floor(Math.random() * (b - a + 1)) + a
+}
+
+
+// link das imagens aqui:
+const imagesGiras = [
+    
+]
+
+
+function replaceImages() {
+  const images = document.getElementsByTagName("img");
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    image.src = imagesGiras[numeroAleatorio(0, imagesGiras.length - 1)];
+  }
+}
+
+function activateGirasMode() {
+  replaceImages();
+}
+
+// Chamada da função principal
+activateGirasMode();
+
