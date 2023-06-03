89dc6bfe9cef37c77f796b1b59aba4c0533ec123
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-06-03 11:11:26 -0300

    alterações

---
+++ 2/borderify.js
@@ -0,0 +1,12 @@
+// document.body.style.border = "5px solid red";
+
+
+const img = document.createElement("img")
+img.src = "./cursor.png"
+
+img.style.width = "100px";
+img.style.height = "100px";
+img.style.position = "absolute";
+
+document.body.appendChild(img);
+
