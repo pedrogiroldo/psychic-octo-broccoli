4b40da4a162ab16b26556af3c33358199f83e6ad
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-06-27 00:30:54 -0300

    Adiciona funcionalidade para substituir imagens novas no DOM e configura um observer para monitorar alterações na árvore DOM.

--- content.js
+++ content.js
@@ -13,8 +13,40 @@ function replaceImages() {
   }
 }
 
+function replaceNewImages(addedNodes) {
+  addedNodes.forEach((node) => {
+    // Se o nó adicionado é uma imagem
+    if (node.nodeType === Node.ELEMENT_NODE && node.tagName === "IMG") {
+      node.src = imgs[numeroAleatorio(0, imgs.length - 1)];
+    }
+    // Verificar se o nó contém imagens
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      const images = node.getElementsByTagName("img");
+      for (let i = 0; i < images.length; i++) {
+        images[i].src = imgs[numeroAleatorio(0, imgs.length - 1)];
+      }
+    }
+  });
+}
+
 function psychicOctoBroccoliMode() {
+  // Substituir imagens existentes
   replaceImages();
+
+  // Configurar observer para monitorar mudanças no DOM
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        replaceNewImages(mutation.addedNodes);
+      }
+    });
+  });
+
+  // Iniciar observação
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 }
 
 // Chamada da função principal
